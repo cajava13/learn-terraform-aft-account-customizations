b8e894051a6f35ab3b4ce2510794f480696a9a31
cajava13 <carlosfernandezarg@gmail.com>
2024-05-15 15:33:14 -0300

    update main.tf

--- Security-Tooling/ACCOUNT_TEMPLATE/terraform/main.tf
+++ Security-Tooling/ACCOUNT_TEMPLATE/terraform/main.tf
@@ -8,14 +8,18 @@ provider "aws" {
 
 # Create KMS service
 module "aws-kms" {
-  source = "your-org-name/kms/aws"
+  # source = "your-org-name/kms/aws"
+  source  = "terraform-aws-modules/kms/aws"
+  version = "3.0.0"
   description             = "My KMS key"
   deletion_window_in_days = 7  
 }
 
 
-module "secrets_manager" {
-  source = "your-org-name/secretmanager/aws"
+module "secrets-manager" {
+  # source = "your-org-name/secretmanager/aws"
+  source  = "terraform-aws-modules/secrets-manager/aws"
+  version = "1.1.2"
   
   name            = "my-secret"
   secret_string   = jsonencode({
