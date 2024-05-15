97d62bb9b0044445a19839d9f1e5e735d3be5ccb
cajava13 <carlosfernandezarg@gmail.com>
2024-05-15 15:18:43 -0300

    update main.tf

--- Security-Tooling/ACCOUNT_TEMPLATE/terraform/main.tf
+++ Security-Tooling/ACCOUNT_TEMPLATE/terraform/main.tf
@@ -13,4 +13,17 @@ module "kms" {
   deletion_window_in_days = 7  
 }
 
+
+module "secrets_manager" {
+  source = "your-org-name/secretmanager/aws"
+  
+  name            = "my-secret"
+  secret_string   = jsonencode({
+      DB_HOST     = "db.example.com",
+      DB_USER     = "user",
+      DB_PASSWORD = "password"
+  })
+}
+
+
 # ```
