007ba3be17a8862ed3b1e7d79c05cb1fff1cc173
cajava13 <carlosfernandezarg@gmail.com>
2024-05-21 13:37:55 -0300

    update main.tf

--- Security-seguridadG/terraform/main.tf
+++ Security-seguridadG/terraform/main.tf
@@ -2,7 +2,7 @@ module "kms" {
   source  = "terraform-aws-modules/kms/aws"
   version = "3.0.0"
 
-  description = "KMS key for encrypting S3 objects." 
+  description = "KMS key for encrypting EBS objects." 
   deletion_window_in_days = 7  
   enable_key_rotation = true
 
