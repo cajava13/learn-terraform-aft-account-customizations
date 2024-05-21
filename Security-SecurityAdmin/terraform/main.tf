0744a2ad1081477e4d2901733c63cb9347dc5f97
cajava13 <carlosfernandezarg@gmail.com>
2024-05-21 13:54:57 -0300

    update main.tf

---
+++ Security-SecurityAdmin/terraform/main.tf
@@ -0,0 +1,14 @@
+# Create KMS service
+module "kms" {
+  source = "./modules/kms"
+  version = "3.0.0"
+  
+  name = "my-secret"
+  description = "This is my secret"
+
+  secret_string = "Shh don't tell anyone!"
+
+  tags = {
+    Environment = "Prod"
+  }
+}
