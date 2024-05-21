79cfa7a2cd6096abd289c233c4f6fa715a5a2037
cajava13 <carlosfernandezarg@gmail.com>
2024-05-20 21:38:30 -0300

    newcode

--- Security-seguridadG/terraform/main.tf
+++ Security-seguridadG/terraform/main.tf
@@ -7,11 +7,11 @@ module "kms" {
   source  = "terraform-aws-modules/kms/aws"
   version = "3.0.0"
 
-  description = "KMS key for encrypting S3 objects" 
+  description = "KMS key for encrypting S3 objects." 
   deletion_window_in_days = 7  
   enable_key_rotation = true
 
   tags = {
-    Environment = "dev"
+    Environment = "Prod"
   }
 }
