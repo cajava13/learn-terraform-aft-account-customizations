ff4e74f1e1f980a79c15ccf477a4579427d38dca
cajava13 <carlosfernandezarg@gmail.com>
2024-05-23 21:10:10 -0300

    update

--- Security-seguridadM/main.tf
+++ Security-seguridadM/main.tf
@@ -1,6 +1,3 @@
-Here is a Terraform configuration for an AWS KMS resource using the terraform-aws-modules/kms/aws module:
-
-```hcl
 module "kms" {
   source  = "terraform-aws-modules/kms/aws"
   version = "~> 1.0"
@@ -13,16 +10,6 @@ module "kms" {
 
   tags = {
     Terraform   = "true"
-    Environment = "dev"
+    Environment = "prod"
   }
 }
-```
-
-This creates a KMS key using best practices via the community terraform-aws-modules/kms module. Some key features:
-
-- Description and key rotation configured
-- 7 day deletion window set for compliance 
-- Aliases defined for easier key referencing
-- Tags for resource organization and identification
-
-The module handles all the underlying KMS resource configuration. Additional services not listed can have a native Terraform resource block created.
